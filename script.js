182845e2cd107020c38acaad372cd10b69356f1b
James Gallagher <jamesg@jamesg.blog>
2023-02-14 16:24:54 +0000

    update banner, fix color bug, add line support

--- script.js
+++ script.js
@@ -27,6 +27,26 @@ var regions = [];
 var masterPoints = [];
 var masterColors = [];
 
+var showNormalized = false;
+var drawMode = "polygon";
+
+var modeMessage = document.querySelector('#mode');
+var coords = document.querySelector('#coords');
+
+// if user presses L key, change draw mode to line and change cursor to cross hair
+document.addEventListener('keydown', function(e) {
+    if (e.key == 'l') {
+        drawMode = "line";
+        canvas.style.cursor = 'crosshair';
+        modeMessage.innerHTML = "Draw Mode: Line (press <kbd>p</kbd> to change to polygon drawing)";
+    }
+    if (e.key == 'p') {
+        drawMode = "polygon";
+        canvas.style.cursor = 'crosshair';
+        modeMessage.innerHTML = 'Draw Mode: Polygon (press <kbd>l</kbd> to change to line drawing)';
+    }
+});
+
 function clipboard(selector) {
     var copyText = document.querySelector(selector).innerText;
     navigator.clipboard.writeText(copyText);
@@ -75,14 +95,13 @@ function drawAllPolygons () {
     // draw all points for previous regions
     for (var i = 0; i < masterPoints.length; i++) {
         var newpoints = masterPoints[i];
+        // set color
+        ctx.strokeStyle = masterColors[i];
         for (var j = 1; j < newpoints.length; j++) {
             // draw all lines
             drawLine(newpoints[j - 1][0], newpoints[j - 1][1], newpoints[j][0], newpoints[j][1]);
         }
-        // close loop
-        if (i != masterPoints.length - 1) {
-            drawLine(newpoints[newpoints.length - 1][0], newpoints[newpoints.length - 1][1], newpoints[0][0], newpoints[0][1]);
-        }
+        drawLine(newpoints[newpoints.length - 1][0], newpoints[newpoints.length - 1][1], newpoints[0][0], newpoints[0][1]);
         // draw arc around each point
         for (var j = 0; j < newpoints.length; j++) {
             ctx.beginPath();
@@ -148,12 +167,19 @@ document.querySelector('#saveImage').addEventListener('click', function(e) {
 
 // on canvas hover, if cursor is crosshair, draw line from last point to cursor
 canvas.addEventListener('mousemove', function(e) {
+    var x = getScaledCoords(e)[0];
+    var y = getScaledCoords(e)[1];
+    // round
+    x = Math.round(x);
+    y = Math.round(y);
+
+    // update x y coords
+    var xcoord = document.querySelector('#x');
+    var ycoord = document.querySelector('#y');
+    xcoord.innerHTML = x;
+    ycoord.innerHTML = y;
+
     if (canvas.style.cursor == 'crosshair') {
-        var x = getScaledCoords(e)[0];
-        var y = getScaledCoords(e)[1];
-        // round
-        x = Math.round(x);
-        y = Math.round(y);
         //ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(img, 0, 0);
         for (var i = 0; i < points.length - 1; i++) {
@@ -167,7 +193,7 @@ canvas.addEventListener('mousemove', function(e) {
             ctx.stroke();
             drawLine(points[i][0], points[i][1], points[i + 1][0], points[i + 1][1]);
         }
-        if (points.length > 0) {
+        if ((points.length > 0 && drawMode == "polygon") || (points.length > 0 && points.length < 2 && drawMode == "line")) {
             ctx.beginPath();
             ctx.strokeStyle = rgb_color;
             ctx.arc(points[i][0], points[i][1], 5, 0, 2 * Math.PI);
@@ -176,15 +202,29 @@ canvas.addEventListener('mousemove', function(e) {
             ctx.fill();
             ctx.stroke();
             drawLine(points[points.length - 1][0], points[points.length - 1][1], x, y);
+
+            if (points.length == 2 && drawMode == "line") {
+                console.log("line");
+                // draw arc around each point
+                ctx.beginPath();
+                ctx.strokeStyle = rgb_color;
+                ctx.arc(points[0][0], points[0][1], 5, 0, 2 * Math.PI);
+                // fill with white
+                ctx.fillStyle = 'white';
+                ctx.fill();
+                ctx.stroke();
+                masterPoints.push(points);
+                points = [];
+            }
         }
-    var parentPoints = [];
+        var parentPoints = [];
 
-    for (var i = 0; i < masterPoints.length; i++) {
-        parentPoints.push(masterPoints[i]);
-    }
-    parentPoints.push(points);
+        for (var i = 0; i < masterPoints.length; i++) {
+            parentPoints.push(masterPoints[i]);
+        }
+        parentPoints.push(points);
 
-    drawAllPolygons();
+        drawAllPolygons();
     }
 });
 
@@ -196,14 +236,17 @@ window.addEventListener('keydown', function(e) {
         // join the dots
         drawLine(points[0][0], points[0][1], points[points.length - 1][0], points[points.length - 1][1]);
         // fill polygon with color
-        ctx.beginPath();
-        ctx.moveTo(points[0][0], points[0][1]);
-        ctx.fillStyle = opaque_color;
-        for (var i = 1; i < points.length; i++) {
-            ctx.lineTo(points[i][0], points[i][1]);
+        if (drawMode == 'polygon') {
+            ctx.beginPath();
+            ctx.moveTo(points[0][0], points[0][1]);
+            ctx.fillStyle = opaque_color;
+            for (var i = 1; i < points.length; i++) {
+                ctx.lineTo(points[i][0], points[i][1]);
+            }
+            ctx.closePath();
+            ctx.fill();
+            // draw line connecting last two points
         }
-        ctx.closePath();
-        ctx.fill();
         masterPoints.push(points);
         // draw arc around last point
         ctx.beginPath();
@@ -215,6 +258,8 @@ window.addEventListener('keydown', function(e) {
         ctx.stroke();
         points = [];
         rgb_color = color_choices[Math.floor(Math.random() * color_choices.length)];
+    
+        masterColors.push(rgb_color);
     }
 });
 
@@ -249,35 +294,29 @@ canvas.addEventListener('drop', function(e) {
         canvas.style.borderRadius = '10px';
         ctx.drawImage(img, 0, 0);
     };
+    // show coords
+    document.getElementById('coords').style.display = 'inline-block';
 });
 
+function writePoints(parentPoints) {
+    var normalized = [];
 
-canvas.addEventListener('click', function(e) {
-    // set cursor to crosshair
-    canvas.style.cursor = 'crosshair';
-    var x = getScaledCoords(e)[0];
-    var y = getScaledCoords(e)[1];
-    x = Math.round(x);
-    y = Math.round(y);
-
-    points.push([x, y]);
-    ctx.beginPath();
-    ctx.strokeStyle = rgb_color;
-    // add rgb_color to masterColors
-    
-    if (masterColors.length != masterPoints.length - 1) {
-        masterColors.push(rgb_color);
+    // if normalized is true, normalize all points
+    var imgHeight = img.height;
+    var imgWidth = img.width;
+    if (showNormalized) {
+        for (var i = 0; i < parentPoints.length; i++) {
+            var normalizedPoints = [];
+            for (var j = 0; j < parentPoints[i].length; j++) {
+                normalizedPoints.push([
+                    Math.round(parentPoints[i][j][0] / imgWidth * 100) / 100,
+                    Math.round(parentPoints[i][j][1] / imgHeight * 100) / 100
+                ]);
+            }
+            normalized.push(normalizedPoints);
+        }
+        parentPoints = normalized;
     }
-
-    ctx.arc(x, y, 155, 0, 2 * Math.PI);
-    // concat all points into one array
-    var parentPoints = [];
-
-    for (var i = 0; i < masterPoints.length; i++) {
-        parentPoints.push(masterPoints[i]);
-    }
-    // add "points"
-    parentPoints.push(points);
 
     // create np.array list
     var code_template = `
@@ -305,4 +344,53 @@ return `{"x": ${point[0]}, "y": ${point[1]}}`;
 }
     `;
     document.querySelector('#json').innerHTML = json_template;
+}
+
+canvas.addEventListener('click', function(e) {
+    // set cursor to crosshair
+    canvas.style.cursor = 'crosshair';
+    // if line mode and two points have been drawn, add to masterPoints
+    if (drawMode == 'line' && points.length == 2) {
+        masterPoints.push(points);
+        points = [];
+    }
+    var x = getScaledCoords(e)[0];
+    var y = getScaledCoords(e)[1];
+    x = Math.round(x);
+    y = Math.round(y);
+
+    points.push([x, y]);
+    ctx.beginPath();
+    ctx.strokeStyle = rgb_color;
+    // add rgb_color to masterColors
+    
+    if (masterColors.length == 0) {
+        masterColors.push(rgb_color);
+    }
+
+    ctx.arc(x, y, 155, 0, 2 * Math.PI);
+    // concat all points into one array
+    var parentPoints = [];
+
+    for (var i = 0; i < masterPoints.length; i++) {
+        parentPoints.push(masterPoints[i]);
+    }
+    // add "points"
+    parentPoints.push(points);
+
+    writePoints(parentPoints);
+});
+
+document.querySelector('#normalize_checkbox').addEventListener('change', function(e) {
+    showNormalized = e.target.checked;
+    // normalize all
+    var parentPoints = [];
+
+    for (var i = 0; i < masterPoints.length; i++) {
+        parentPoints.push(masterPoints[i]);
+    }
+
+    parentPoints.push(points);
+
+    writePoints(parentPoints);
 });
